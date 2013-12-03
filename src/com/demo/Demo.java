8a0d5a57a28b421092c1e97bea8a5e71d908fde7
FishChang <h225030919@gmail.com>
2013-12-03 22:52:43 +0800

    first

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.demo;
+public class Demo{
+public static void main(String[] args) {
+        Cat myCat = new Cat();
+        Animal myAnimal = myCat;
+		Animal.testClassMethod();
+		
+        myAnimal.testClassMethod();
+        myAnimal.testInstanceMethod();
+		myCat.testClassMethod();
+		myCat.testInstanceMethod();
+		
+    }
+}
---
+++ src/com/demo/HelloWorld.java
@@ -0,0 +1,5 @@
+package com.demo;
+public class HelloWorld{
+	public	static void main(String []args){
+			System.out.println("Hello World");}
+}
